22fda0ce96de99495661addfc2c39eacb6d3bb08
Théo Grollier <theo.grollier85@gmail.com>
2022-06-09 21:45:29 +0200

    Song cards

--- front/components/audioCard/AudioCard.js
+++ front/components/audioCard/AudioCard.js
@@ -1,9 +1,19 @@
 import styles from './AudioCard.module.css'
+import Image from 'next/image'
+import firstImg from '../../public/export/Image3.png'
 
-function AudioCard() {
+function AudioCard({ song, artist, img }) {
   return(
     <div className={styles.audioCardContainer}>
-      
+      <Image className={styles.audioCardImage} src={img} height={156} width={156}></Image>
+      <div className={styles.audioCardInfo}>
+        <p className={styles.song}>
+          {song}
+        </p>
+        <p className={styles.artist}>
+          {artist}
+        </p>
+      </div>
     </div>
   )
 }
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,24 @@
 import AppBar from '../components/appbar/AppBar'
+import AudioCard from '../components/audioCard/AudioCard'
 import Player from '../components/player/Player'
+import styles from '../styles/Home.module.css'
+import imgFirst from '../public/export/Image3'
+import imgSecond from '../public/export/Image4.png'
+import imgThird from '../public/export/Image5.png'
+import imgFourth from '../public/export/Image6.png'
+
 
 export default function Home () {
   return (
     <>
       <AppBar></AppBar>
+      <div className={styles.songContainer}>
+        <AudioCard song='Test' artist='Test' img={imgFirst}></AudioCard>
+        <AudioCard song='Test' artist='Test' img={imgSecond}></AudioCard>
+        <AudioCard song='Test' artist='Test' img={imgThird}></AudioCard>
+        <AudioCard song='Test' artist='Test' img={imgFourth}></AudioCard> 
+      </div>
+
       <Player></Player>
     </>
   )
