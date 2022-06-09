d8ab3bccb178d55a4390703b9824ca5481a105f4
Théo Grollier <theo.grollier85@gmail.com>
2022-06-09 22:17:32 +0200

    Btn voir plus et title categorie

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,11 +2,13 @@ import AppBar from '../components/appbar/AppBar'
 import AudioCard from '../components/audioCard/AudioCard'
 import Player from '../components/player/Player'
 import styles from '../styles/Home.module.css'
-import imgFirst from '../public/export/Image3'
+import imgFirst from '../public/export/Image3.png'
 import imgSecond from '../public/export/Image4.png'
 import imgThird from '../public/export/Image5.png'
 import imgFourth from '../public/export/Image6.png'
-
+import VoirPlus from '../components/voirplus/voir_plus'
+import AnnonceCategorie from '../components/categorie_actualite/annonce_categorie'
+import zikmu from '../public/export/Zikmu.png'
 
 export default function Home () {
   return (
@@ -18,7 +20,10 @@ export default function Home () {
         <AudioCard song='Test' artist='Test' img={imgThird}></AudioCard>
         <AudioCard song='Test' artist='Test' img={imgFourth}></AudioCard> 
       </div>
-
+      <div className={styles.voirPlusSongs}>
+        <VoirPlus></VoirPlus>
+      </div>
+      <AnnonceCategorie title='Test' icon={zikmu}></AnnonceCategorie>
       <Player></Player>
     </>
   )
