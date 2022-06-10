141d775b2015ced9d6886bfe1689ead60535883c
Noudea <oudea.nicolas@gmail.com>
2022-06-10 12:26:16 +0200

    last push

---
+++ front/pages/podcast.js
@@ -0,0 +1,11 @@
+import styles from '../styles/podcastScreen.module.css'
+
+function podcast() {
+  return(
+    <div className={styles.podcastScreenContainer}>
+
+    </div>
+  )
+}
+
+export default podcast
